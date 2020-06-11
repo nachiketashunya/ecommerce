1adc1896c1ff883644c4cd385dd159eb084a8a17
Nachiketa <nachiketapurohit.2611@gmail.com>
2020-06-11 16:20:52 +0530

    Commented on search/view.py

--- search/views.py
+++ search/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render
 from products.models import SubCategory, Product
-from django.db.models import Q 
+from django.db.models import Q
 # Create your views here.
+# Q objects are used to execute more complex queries with OR statement
 
+#View function to use search functionality
 def search(request):
 	method_dict = request.GET
-	query = method_dict.get('search')
+	query = method_dict.get('search')	#name of search bar
 	subcategory = SubCategory.objects.all()
 
 	if query is not None:
-		lookups = (Q (description__icontains = query) | 
-					Q (brand_name__brand_name__icontains = query) | 
+		lookups = (Q (description__icontains = query) |							#icontains -- case insensitive, used with double(__)
+					Q (brand_name__brand_name__icontains = query) |
 					Q (title__icontains = query)
 				)
 
-		products = Product.objects.filter(lookups).distinct
-		context = { 'products' : products , 'subcategory': subcategory}
+		products = Product.objects.filter(lookups).distinct		#distince is used to prevent duplication
+		context = {
+					'products' : products,
+					'subcategory' : subcategory
+				}
 
 	else:
 		products = Product.objects.all()
-		context = { 'products' : products , 'subcategory': subcategory}
+
+		context = {
+					'products' : products,
+					'subcategory' : subcategory
+				}
 
 	return render(request, 'front/productpage.html', context)
- 
+
 
